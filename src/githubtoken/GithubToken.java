2ee9fa69e0263644a1e3d285361271eac79e4c40
vieira3030 <vieirarodrigo@ipvc.pt>
2026-03-03 20:37:31 +0000

    first commit

---
+++ src/githubtoken/GithubToken.java
@@ -0,0 +1,12 @@
+package githubtoken;
+
+/**
+ * @author rfcm29
+ */
+public class GithubToken {
+
+    public static void main(String[] args) {
+        System.out.println("GithubToken Project");
+    }
+    
+}
